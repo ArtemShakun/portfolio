2fcca6c6486cd5285e32c44684ec9dd3b8fbdc67
Artem Shakun <artem.shakun.91@gmail.com>
2024-09-27 21:16:02 +0300

    added grid for about page

--- src/pages/about/about-page.tsx
+++ src/pages/about/about-page.tsx
@@ -1,17 +1,42 @@
 import { About } from '@/types/aboutpage-type';
 
 import styles from './aboutpage.module.css';
+import { ProfileComponent } from '../homepage/components/profile/profile';
+import { TogetherBlock } from '../homepage/components/together/together';
 
 type AboutPageProps = {
   data: About;
 };
 
 export const AboutPage = ({ data }: AboutPageProps) => {
+  const profile = {
+    title: 'Profiles',
+    sub_title: 'Stay with me',
+    link: '/contact-me',
+  };
+
+  const together = {
+    title: 'Let&apos;s <br /> work <span>together</span>',
+    link: '/contact-me',
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.upperSection}></div>
-      <div className={styles.middleSection}></div>
-      <div className={styles.bottomSection}></div>
+      <div className={styles.upperSection}>
+        <div className={styles.photo} />
+        <div className={styles.wrapper}>
+          <div className={styles.title}></div>
+          <div className={styles.userInfo}></div>
+        </div>
+      </div>
+      <div className={styles.middleSection}>
+        <div className={styles.experience}></div>
+        <div className={styles.education}></div>
+      </div>
+      <div className={styles.bottomSection}>
+        <ProfileComponent profile={profile} />
+        <TogetherBlock together={together} />
+        <div className={styles.userInfo}></div>
+      </div>
     </div>
   );
 };
