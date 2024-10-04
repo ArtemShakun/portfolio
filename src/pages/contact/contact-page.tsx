b72f3c1fccd44215723ba3b04d072c5940640d9b
Artem Shakun <artem.shakun.91@gmail.com>
2024-10-04 17:33:33 +0300

    added contact info

--- src/pages/contact/contact-page.tsx
+++ src/pages/contact/contact-page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import mailIcon from '@/img/contact/mail.svg';
+import phoneIcon from '@/img/contact/phone.svg';
 import { Contact } from '@/types/contactpage-type';
 import styles from './contact.module.css';
 
@@ -8,7 +11,20 @@ type ContactPageProps = {
 export const ContactPage = ({ data }: ContactPageProps) => {
   return (
     <div className={styles.container}>
-      <div className={styles.informationBlock}></div>
+      <div className={styles.informationBlock}>
+        <h1 className={styles.nameCategory}> Contact Info</h1>
+        {data.contacts_data.map(item => (
+          <div key={item.id} className={styles.infoCard}>
+            <div className={styles.imgBlock}>
+              <Image src={mailIcon} alt="social icon" />
+            </div>
+            <div className={styles.textBlock}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.subTitle}>{item.info}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       <div className={styles.formBlock}></div>
     </div>
   );
